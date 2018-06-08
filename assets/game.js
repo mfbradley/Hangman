a7c12259feed2f532434d0841df31500369e1532
mfbradley <mbradleystylist@gmail.com>
2018-06-08 16:45:25 -0400

    added buttons for each letter

--- assets/game.js
+++ assets/game.js
@@ -9,8 +9,35 @@ var lettersAndBlanks = [];
 var wordArray = ["grand canyon", "zion", "yellowstone", "yosemite"];
 var wordToGuess;
 var userGuess;
+var letter = ""
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+for (var i = 0; i < alphabet.length; i++) {
+        var button = $("<button type='button' class='btn btn-light' data-value='" + alphabet[i] + "'></button>");
+        button.text(alphabet[i]);
+        if (i < 4) {
+            $(".buttonsDiv1").append(button);
+        }
+        else if (i >= 4 && i < 9) {
+            $(".buttonsDiv2").append(button);
+        }
+        else if (i >= 9 && i < 15 ) {
+            $(".buttonsDiv3").append(button);
+        }
+
+        else if (i >= 15 && i < 20 ) {
+            $(".buttonsDiv4").append(button);
+        }
+
+        else {
+            $(".buttonsDiv5").append(button);
+        }
+    }
+
 
 $(document).ready(function () {
+  
     // when game begins (if guesses left === 0)...
     // set wordToGuess to a random word in array
     // push blanks into commas array (array with commas)
@@ -38,14 +65,14 @@ $(document).ready(function () {
     // guessesRemaining--
     // replace text of lettersGuessedDiv with lettersSoFar array
     // loop over wordToGuess and see if the user guess matches any of the letters in the word
-    
+
     document.onkeyup = function (event) {
         var userGuess = event.key.toLowerCase();
         console.log(userGuess);
 
         if (lettersSoFar.indexOf(userGuess) < 0) {
             lettersSoFar.push(userGuess);
-            
+
 
             guessesRemaining--;
 
@@ -57,8 +84,8 @@ $(document).ready(function () {
         }
 
 
-        for (var i=0; i < wordBlanks.length; i++) {
-                
+        for (var i = 0; i < wordBlanks.length; i++) {
+
             if (userGuess != wordToGuess[i] && lettersAndBlanks[i] != wordToGuess[i]) {
                 lettersAndBlanks[i] = "_";
             }
@@ -69,7 +96,7 @@ $(document).ready(function () {
                 console.log(lettersAndBlanks);
             }
 
-            
+
         }
         var noCommas = lettersAndBlanks.join(" ");
         $(".wordBlanksHere").text(noCommas);
